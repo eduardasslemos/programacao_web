3381c5707df837279b89e47586c78996d2588d32
eduardasslemos <eduardasslemos@gmail.com>
2026-03-19 22:02:04 -0300

    exercicio aula8

--- aula8/src/exercicioAula8.ts
+++ aula8/src/exercicioAula8.ts
@@ -4,7 +4,33 @@ type Tarefa = {
     concluida: boolean;
 };
 
-/*function executarTarefas (objetos: Tarefa[], callback: (param: string, number, number) => void, intervalo: number = 1000): void{
+const tarefas: Tarefa[] = [
+{descricao: "Fazer relatório", prioridade: 2, concluida: false },
+{descricao: "Enviar e-mail", prioridade: 3, concluida: false },
+{descricao: "Reunião com equipe", prioridade: 1, concluida: false }
+];
 
-    callback(descricao, indice, quantidade);
-}*/
+function imprimirTarefa(descricao: string, indice: number, totalTarefas: number): void{
+    console.log (`Tarefa concluída: ${descricao}`) ;
+    console.log (`Progresso: ${indice + 1}/ ${totalTarefas}`) ;
+}
+
+function executarTarefas(tarefas: Tarefa[], callback: (descricao: string, indice: number, totalTarefas: number) => void, timer: number = 1000){
+    tarefas.sort((a, b) => b.prioridade - a.prioridade); //decrescente
+    let contador = 0;
+
+    const interval = setInterval(() => {
+        if(contador >= tarefas.length || tarefas[contador]?.descricao === "Cancelar"){ //se o contador for maior doq a quantidade de coisas na tarefas ou a descricao da tarefa atual for cancelar ent paramos o interval
+            clearInterval(interval);
+        } else {
+            const tarefa = tarefas[contador];
+            if(tarefa){ //se n for nula entra no if
+                tarefa.concluida = true; //muda a tarefa p concluida
+                callback(tarefa.descricao, contador, tarefas.length); //mostra a funcao imprimir tarefa
+                contador++; //aumenta +1 no contador
+            }
+        }
+    }, timer);
+}
+
+executarTarefas(tarefas, imprimirTarefa, 1500);
